9f544186027333c8d3a2a2faa9aa1493d29afb93
AryanSinghal1 <88899023+AryanSinghal1@users.noreply.github.com>
2024-08-13 14:40:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-import server from "./server.js";
+import express from 'express';
+import  createAfile from './tools/createAfile.js';
+import getAFile from './tools/getAFile.js';
+import getAllFiles from './tools/getAllFilename.js';
+import updateAfile from './tools/updateAfile.js';
+import deleteAFile from './tools/deleteAFile.js';
+const app = express();
+const port = 8000;
 
-server.listen(3000, () => {
-  console.log("server started");
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
 });
